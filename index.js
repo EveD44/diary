eeb48ceb41aaddfe5ab82f60076f821333109979
evedavies <evedavies@Arthurs-MacBook-Air.local>
2024-06-05 13:34:27 +0100

    Adding functionality to create new post and add to json file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import  entries_data from './diary-entries.json'  with { type: "json" };
+import * as fs from "fs";
 
 const app = express();
 const port = 3000;
@@ -8,7 +9,42 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Add Entry
+function addEntry(inputTitle, inputContent) {
+
+    const d = new Date();
+    const newID = (entries_data[entries_data.length - 1].id) + 1;
+
+    var entry = {
+        id: newID,
+        title: inputTitle,
+        content: inputContent,
+        date: {
+            year: d.getFullYear(),
+            month: d.getMonth(), 
+            day: d.getDay()
+        }
+    }
+
+    // Adding new data to entries object 
+    entries_data.push(entry);
+
+    // Writing to a file 
+    fs.writeFile(
+        "diary-entries.json",
+        JSON.stringify(entries_data),
+        err => {
+            // Checking for errors 
+            if (err) throw err;
+
+            // Success 
+            console.log("Done writing");
+        }); 
+}
+
+
 let entry_data;
+let entry;
 //console.log(entries_data[1]);
 
 app.get("/", (req, res) => {
@@ -28,7 +64,12 @@ app.get("/new-post", (req, res) => {
     res.render("add-post.ejs");
 })
 
-
+app.post("/submit", (req, res) => {
+    let title = req.body.title;
+    let content = req.body.content;
+    addEntry(title, content);
+    res.redirect("/new-post")
+})
 
 
 app.listen(port, () => {
